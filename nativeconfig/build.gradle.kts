bbea0109420c8ba5322c8b19a94f002db1d4f706
Muhamad Syafii <muhamadsyafii4@gmail.com>
2025-08-11 11:33:17 +0700

    Feat : (#1)
    - setup rust language
    - add ndk version
    - add native lib module
    - add class object for using rust language or get value from rust
    - add how to use on mainactivity

--- nativeconfig/build.gradle.kts
+++ nativeconfig/build.gradle.kts
@@ -1,20 +1,21 @@
 plugins {
-  alias(libs.plugins.android.application)
+  alias(libs.plugins.android.library)
   alias(libs.plugins.kotlin.android)
 }
 
 android {
-  namespace = "dev.syafii.rustlab"
+  namespace = "dev.syafii.rustlab.nativeconfig"
   compileSdk = 35
 
   defaultConfig {
-    applicationId = "dev.syafii.rustlab"
     minSdk = 24
-    targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    ndkVersion = "26.1.10909125"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
   }
+
+  sourceSets["main"].jniLibs.srcDir("src/main/jniLibs")
+
 
   buildTypes {
     release {
@@ -29,20 +30,16 @@ android {
   kotlinOptions {
     jvmTarget = "17"
   }
-  buildFeatures {
-    viewBinding = true
-    buildConfig = true
-  }
 }
 
 dependencies {
-  implementation(project(":nativeconfig"))
-  implementation(libs.androidx.core.ktx)
+  implementation(kotlin("stdlib"))
+
+
+/*  implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.appcompat)
   implementation(libs.material)
-  implementation(libs.androidx.activity)
-  implementation(libs.androidx.constraintlayout)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
-  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.espresso.core)*/
 }
